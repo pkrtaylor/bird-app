67e2fd29f3d73369160b01b519b09a9c404a40b9
Pierce Taylor <piercetaylor98@gmail.com>
2023-08-04 21:59:31 -0400

    merging of master into main

--- frontend/src/components/Alert.js
+++ frontend/src/components/Alert.js
@@ -8,8 +8,8 @@ const Alert = () => {
     return(
         alerts !== null && alerts.length > 0 
             && alerts.map(alert => (
-                <div className=" w-screen flex flex-col items-center">
-                    <div key={alert.id} className="newAlert flex justify-center items-center text-white border-2 border-blue-600 bg-blue-950 rounded-lg w-[300px] h-[50px] ">{alert.msg}</div>
+                <div  key={alert.id} className=" w-screen flex flex-col items-center">
+                    <div className="newAlert flex justify-center items-center text-white border-2 border-blue-600 bg-blue-950 rounded-lg w-[300px] h-[50px] ">{alert.msg}</div>
                 </div>
             ))
     )
