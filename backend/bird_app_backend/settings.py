ed78511168721a0e736a461d790ef55f5481bdbf
Pierce Taylor <piercetaylor98@gmail.com>
2023-08-07 10:27:15 -0400

    editProfile fix and border removals

--- backend/bird_app_backend/settings.py
+++ backend/bird_app_backend/settings.py
@@ -41,12 +41,12 @@ environ.Env.read_env(BASE_DIR / '.env')
 SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+# DEBUG = False
 
-ALLOWED_HOSTS = ['bird-app-snowy.vercel.app']
-# DEBUG = True
+# ALLOWED_HOSTS = ['bird-app-snowy.vercel.app']
+DEBUG = True
 
-# ALLOWED_HOSTS = []
+ALLOWED_HOSTS = []
 
 
 # Application definition
